60a031cf78efd3dec21b2d9206236a154cd8ac5d
Patricia Hurst <p.hurst.dev@gmail.com>
2022-04-19 17:14:29 -0400

    updated safe-mode working

--- src/components/jokes/JokesSelect.js
+++ src/components/jokes/JokesSelect.js
@@ -1,18 +1,14 @@
-import { useState, useRef, useContext } from 'react';
+import { useState, useContext } from 'react';
 import JokeContext from '../../context/JokeContext';
 import { fetchJokes } from '../../context/JokesActions';
 import Wrapper from '../ui/Wrapper';
 
-// TODO: switch select dropdown to useRef as well (useState only for btnDisabled state)
-
 const JokesSelect = () => {
+  const [btnDisabled, setBtnDisabled] = useState(false);
   const [formData, setFormData] = useState({
-    selected: '0',
-    btnDisabled: true,
+    selected: 'Misc',
+    safemodeChecked: false,
   });
-
-  // useRef to insure that checkbox state is up to date for button disabling validations
-  const checkboxRef = useRef();
 
   const { jokes, dispatch } = useContext(JokeContext);
 
@@ -21,38 +17,43 @@ const JokesSelect = () => {
 
     dispatch({ type: 'SET_LOADING' });
 
-    let jokes;
-
-    // change API url if safemode is checked:
-    if (checkboxRef.current.checked) {
-      jokes = await fetchJokes(`${formData.selected}?safe-mode`);
-    } else {
-      jokes = await fetchJokes(formData.selected);
-    }
+    const jokes = await fetchJokes(formData.safemodeChecked ? `${formData.selected}?safe-mode` : formData.selected);
 
     dispatch({ type: 'GET_JOKES', payload: jokes });
   };
 
   const handleChange = e => {
-    const { name, value, type } = e.target;
+    const selection = e.target.value;
 
-    const setForm = bool => {
-      setFormData(prevFormData => {
-        return {
-          ...prevFormData,
-          [name]: type === 'checkbox' ? checkboxRef : value,
-          btnDisabled: bool,
-        };
-      });
-    };
+    console.log(selection);
 
-    // set btnDisabled to true if selected value is "0" (the default --) or if safe mode is on and dark jokes are selected:
-    if (value === '0' || (value === 'Dark' && checkboxRef.current.checked)) {
-      setForm(true);
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      selected: selection,
+    }))
+
+    if (formData.safemodeChecked && selection === "Dark") {
+      setBtnDisabled(true);
     } else {
-      setForm(false);
+      setBtnDisabled(false);
     }
   };
+
+  const handleCheckbox = e => {
+    console.log(e.target.checked);
+    const isOn = e.target.checked;
+
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      safemodeChecked: isOn,
+    }))
+
+    if(isOn && formData.selected === "Dark") {
+      setBtnDisabled(true);
+    } else {
+      setBtnDisabled(false)
+    }
+  }
 
   return (
     <Wrapper>
@@ -72,7 +73,6 @@ const JokesSelect = () => {
             value={formData.selected}
             className="px-2 py-2 border-solid border-2 border-transparent rounded focus:text-gray-700 focus:border-teal-500 focus:outline-none hover:border-teal-500 mr-6"
           >
-            <option value="0">--</option>
             <option value="Dark">Dark</option>
             <option value="Spooky">Spooky</option>
             <option value="Pun">Pun</option>
@@ -84,7 +84,7 @@ const JokesSelect = () => {
             <button
               type="submit"
               className="btn btn-outline btn-accent"
-              disabled={formData.btnDisabled}
+              disabled={btnDisabled}
             >
               Get Joke
             </button>
@@ -98,12 +98,12 @@ const JokesSelect = () => {
             type="checkbox"
             id="safemodeChecked"
             name="safemodeChecked"
-            ref={checkboxRef}
-            onChange={handleChange}
+            checked={formData.safemodeChecked}
+            onChange={handleCheckbox}
           />
           <p className="w-96 mx-auto">
             Use safe mode to exclude all dark category jokes, as well as nsfw,
-            explicit and other "unsafe" jokes.
+            explicit and other "unsafe" jokes from all categories.
           </p>
         </div>
       </form>
