a1298f2098ae2f723987d9c266f1e2d736e79dfe
Patricia Hurst <p.hurst.dev@gmail.com>
2022-04-20 18:52:11 -0400

    update

--- src/components/jokes/JokesResults.js
+++ src/components/jokes/JokesResults.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import JokeContext from '../../context/JokeContext';
 import Spinner from '../ui/Spinner';
 import Wrapper from '../ui/Wrapper';
+import {motion, AnimatePresence} from "framer-motion";
 
 const JokesResults = () => {
   const { jokes, loading } = useContext(JokeContext);
@@ -10,14 +11,23 @@ const JokesResults = () => {
   // layout determined by type of joke (single vs twopart)
   if (jokes.type && !loading) {
     return (
-      <Wrapper>
-        <div className="card drop-shadow-lg bg-teal-600 text-center text-primary-content w-96">
-          <div className="card-body">
-            {jokes.type === "single" && <h2>{jokes.joke}</h2>}
-            {jokes.type === "twopart" && <><h2 className='font-bold text-xl'>{jokes.setup}</h2><p>{jokes.delivery}</p></>}
-          </div>
-        </div>
-      </Wrapper>
+      <AnimatePresence>
+        <motion.div
+          key={jokes.id}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+        >
+          <Wrapper>
+            <div className="card drop-shadow-lg bg-teal-600 text-center text-primary-content w-96">
+              <div className="card-body">
+                {jokes.type === "single" && <h2>{jokes.joke}</h2>}
+                {jokes.type === "twopart" && <><h2 className='font-bold text-xl'>{jokes.setup}</h2><p>{jokes.delivery}</p></>}
+              </div>
+            </div>
+          </Wrapper>
+        </motion.div>
+      </AnimatePresence>
     );
   } else if (loading) {
     return <Spinner />;
