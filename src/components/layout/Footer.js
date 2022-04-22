bebbcd45e9af492dba4aae1a791c45fba05258a1
Patricia Hurst <p.hurst.dev@gmail.com>
2022-04-22 19:28:48 -0400

    remove fixed bottom

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,6 @@ const Footer = () => {
   const footerYear = new Date().getFullYear();
 
   return (
-    // <footer className="fixed bottom-0 w-full text-center bg-teal-600 text-slate-200 dark:border-transparent dark:bg-neutral py-6">
     <footer className="text-center bg-teal-600 text-slate-200 dark:border-transparent dark:bg-neutral py-6">
       <p className="font-bold">
         Copyright &copy; {footerYear} All rights reserved.
