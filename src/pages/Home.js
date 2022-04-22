bb888ac9dc0b0ccf3a8a54a109f6ee75608809d0
Patricia Hurst <p.hurst.dev@gmail.com>
2022-04-22 19:26:59 -0400

    update wrapper to fix mobile footer padding issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Wrapper from '../components/ui/Wrapper';
 
 const Home = () => {
   return (
-    <main className="mt-12 px-2 grow overflow-x-hidden">
+    <main>
       <Wrapper>
         <JokesSelect />
         <JokesResults />
